655fa666d0b15f12a048292bff37d50abe43cc88
yashikawa <yashikawa.7@gmail.com>
2016-05-22 03:58:09 +0900

    add zsh-completions
    diff --git a/.zsh/zplug.zsh b/.zsh/zplug.zsh
    index 42a3d59..c3d2fba 100644
    --- a/.zsh/zplug.zsh
    +++ b/.zsh/zplug.zsh
    @@ -3,3 +3,4 @@ zplug 'themes/daveverwer', from:oh-my-zsh
     zplug "mollifier/cd-gitroot", as:command
     zplug "mollifier/anyframe"
     zplug "peco/peco", as:command, from:gh-r
    +zplug "zsh-users/zsh-completions"

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -3,3 +3,4 @@ zplug 'themes/daveverwer', from:oh-my-zsh
 zplug "mollifier/cd-gitroot", as:command
 zplug "mollifier/anyframe"
 zplug "peco/peco", as:command, from:gh-r
+zplug "zsh-users/zsh-completions"
