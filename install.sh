9693dd0d3067abc98c4b9df3c319f35769b2bdfc
yashikawa <yashikawa.7@gmail.com>
2016-05-22 14:40:54 +0900

    [from now] 2016/05/22 14:40:54
    diff --git a/install.sh b/install.sh
    index 596ae6f..4a10691 100644
    --- a/install.sh
    +++ b/install.sh
    @@ -7,6 +7,7 @@ git submodule update
     for f in .??*
     do
         [[ "$f" == ".git" ]] && continue
    +    [[ "$f" == ".gitmodules" ]] && continue
         [[ "$f" == ".DS_Store" ]] && continue
         ln -snfv "$DOTPATH/$f" "$HOME/$f"
     done

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ git submodule update
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".gitmodules" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
     ln -snfv "$DOTPATH/$f" "$HOME/$f"
 done
